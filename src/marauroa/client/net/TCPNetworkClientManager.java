10c707c927e27db2db1a19fa8edc70d70837a938
ZEKKEQ <45718921+ZEKKEQ@users.noreply.github.com>
2020-09-12 19:05:34 +0200

    Update marauroa

--- src/marauroa/client/net/TCPNetworkClientManager.java
+++ src/marauroa/client/net/TCPNetworkClientManager.java
@@ -26,6 +26,7 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 import marauroa.common.Log4J;
+import marauroa.common.TimeoutConf;
 import marauroa.common.Utility;
 import marauroa.common.net.Decoder;
 import marauroa.common.net.Encoder;
@@ -133,7 +134,7 @@ public final class TCPNetworkClientManager implements INetworkClientManagerInter
 		} else {
 			socket = new Socket(proxy);
 		}
-		socket.connect(address);
+		socket.connect(address, TimeoutConf.SOCKET_TIMEOUT * 1000);
 		socket.setTcpNoDelay(true); // disable Nagle's algorithm
 		socket.setReceiveBufferSize(128 * 1024);
 
@@ -218,7 +219,15 @@ public final class TCPNetworkClientManager implements INetworkClientManagerInter
 		}
 	}
 
-
+	/**
+	 * gets all messages received so far and removes them from the queue.
+	 *
+	 *
+	 * @param drainTo the messages received
+	 */
+	public synchronized void retrieveMessages(Collection<? super Message> drainTo) {
+		 processedMessages.drainTo(drainTo);
+	}
 
 	/**
 	 * gets all messages received so far and removes them from the queue.
@@ -226,6 +235,7 @@ public final class TCPNetworkClientManager implements INetworkClientManagerInter
 	 *
 	 * @return the messages received
 	 */
+	@Deprecated
 	public synchronized Collection<Message> getMessages() {
 		Collection<Message> col = new LinkedList<Message>();
 		 processedMessages.drainTo(col);
